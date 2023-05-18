1804a1bf135567f7fc6ddfef4cd8d4210b03d6bc
Harmanen <hurmanen@gmail.com>
2023-05-18 20:52:30 +0300

    Replace plot with price per litre over time

--- app.R
+++ app.R
@@ -1,49 +1,21 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(plotly)
 
-# Define UI for application that draws a histogram
+data <- read.csv("data/dataset.csv")
+
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Gasoline visualizer 9000"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+    plotlyOutput("plot")
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$plot <- renderPlotly({
+        plot_ly(data, type = "scatter", mode = "lines+markers") %>%
+        add_trace(x = ~Date, y = ~PricePerLitre) %>% 
+        layout(showlegend = FALSE)
     })
 }
 
