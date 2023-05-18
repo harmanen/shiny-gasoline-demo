e3959c0effea8f9ebdf39040684e3e16e71eb02d
Harmanen <hurmanen@gmail.com>
2023-05-18 21:33:58 +0300

    Add color to markers, run styler

--- app.R
+++ app.R
@@ -4,20 +4,25 @@ library(plotly)
 data <- read.csv("data/dataset.csv")
 
 ui <- fluidPage(
-
-    titlePanel("Gasoline visualizer 9000"),
-
-    plotlyOutput("plot")
+  titlePanel("Gasoline visualizer 9000"),
+  plotlyOutput("plot")
 )
 
 server <- function(input, output) {
-
-    output$plot <- renderPlotly({
-        plot_ly(data, type = "scatter", mode = "lines+markers") %>%
-        add_trace(x = ~Date, y = ~PricePerLitre) %>% 
-        layout(showlegend = FALSE)
-    })
+  output$plot <- renderPlotly({
+    plot_ly(data, 
+            type = "scatter", 
+            mode = "lines") %>%
+      add_trace(x = ~Date, 
+                y = ~PricePerLitre,
+                name = "trace", 
+                line = list(color = "gray", dash = "dash")) %>%
+      add_markers(x = ~Date, 
+                  y = ~PricePerLitre,
+                  color = ~City,
+                  marker = list(size = 10))
+  })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
