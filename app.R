554cf474c25bbfe861f733ca89caef6d195bd6a3
Harmanen <hurmanen@gmail.com>
2023-05-31 21:00:27 +0300

    Run styler

--- app.R
+++ app.R
@@ -8,8 +8,14 @@ LABEL_LOCALE <- "en_UK"
 
 # Read data, add day and month columns
 data <- read.csv("data/dataset.csv") %>%
-  mutate(Day = wday(Date, label = TRUE, locale = LABEL_LOCALE)) %>% 
-  mutate(Month = month(Date, label = TRUE, locale = LABEL_LOCALE))
+  mutate(Day = wday(Date,
+    label = TRUE,
+    locale = LABEL_LOCALE
+  )) %>%
+  mutate(Month = month(Date,
+    label = TRUE,
+    locale = LABEL_LOCALE
+  ))
 
 ui <- fluidPage(
   titlePanel("Gasoline visualizer 9000"),
@@ -18,17 +24,22 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   output$plot <- renderPlotly({
-    plot_ly(data, 
-            type = "scatter", 
-            mode = "lines") %>%
-      add_trace(x = ~Date, 
-                y = ~PricePerLitre,
-                name = "trace", 
-                line = list(color = "gray", dash = "dash")) %>%
-      add_markers(x = ~Date, 
-                  y = ~PricePerLitre,
-                  color = ~City,
-                  marker = list(size = 10))
+    plot_ly(data,
+      type = "scatter",
+      mode = "lines"
+    ) %>%
+      add_trace(
+        x = ~Date,
+        y = ~PricePerLitre,
+        name = "trace",
+        line = list(color = "gray", dash = "dash")
+      ) %>%
+      add_markers(
+        x = ~Date,
+        y = ~PricePerLitre,
+        color = ~City,
+        marker = list(size = 10)
+      )
   })
 }
 
