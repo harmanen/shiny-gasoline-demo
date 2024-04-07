e46815efa1ffe4feeeb73d02927d2f931741189a
Harmanen <hurmanen@gmail.com>
2024-04-07 10:57:28 +0300

    Format code

--- app.R
+++ app.R
@@ -16,73 +16,74 @@ ymax2BoxPlot <- 40 #40
 # Read data, add day and month columns
 data <- read.csv("data/dataset.csv") %>%
   mutate(Day = wday(Date,
-    label = TRUE,
-    locale = LABEL_LOCALE
-  )) %>%
+                    label = TRUE,
+                    locale = LABEL_LOCALE)) %>%
   mutate(Month = month(Date,
-    label = TRUE,
-    locale = LABEL_LOCALE
-  ))
+                       label = TRUE,
+                       locale = LABEL_LOCALE))
 
 ui <- fluidPage(
   titlePanel("Gasoline visualizer 9000"),
   
-  fluidRow(column(12,plotlyOutput("scatterPlot"))),
-  fluidRow(column(6,plotlyOutput("boxPlot")),
-           column(6,plotlyOutput("barPlot"))
-           )
+  fluidRow(column(12, plotlyOutput("scatterPlot"))),
+  fluidRow(column(6, plotlyOutput("boxPlot")),
+           column(6, plotlyOutput("barPlot")))
 )
 
 server <- function(input, output) {
   output$scatterPlot <- renderPlotly({
     plot_ly(data,
-      type = "scatter",
-      mode = "lines"
-    ) %>%
+            type = "scatter",
+            mode = "lines") %>%
       add_trace(
-        x = ~Date,
-        y = ~PricePerLitre,
+        x = ~ Date,
+        y = ~ PricePerLitre,
         name = "trace",
         line = list(color = "gray", dash = "dash")
       ) %>%
       add_markers(
-        x = ~Date,
-        y = ~PricePerLitre,
-        color = ~City,
+        x = ~ Date,
+        y = ~ PricePerLitre,
+        color = ~ City,
         marker = list(size = 10)
-      ) %>% 
+      ) %>%
       layout(title = "Time series")
   })
   
   output$boxPlot <- renderPlotly({
-    plot_ly(data,
-            y = ~PricePerLitre,
-            boxpoints = "all",
-            jitter = 0.1,
-            name = "Price per litre (€)",
-            notched = TRUE,
-            type = "box"
-            ) %>%
-      add_boxplot(y = ~Volume,
-                  yaxis = "y2",
-                  boxpoints = "all",
-                  jitter = 0.1,
-                  name = "Volume (l)",
-                  notched = TRUE
-                  ) %>%
-      layout(title = "Box plots",
-             showlegend = FALSE,
-             yaxis = list(title = "",
-                          range = list(ymin1BoxPlot, ymax1BoxPlot)
-                          ), 
-             yaxis2 = list(title = "",
-                           range = list(ymin2BoxPlot, ymax2BoxPlot), 
-                           side = "right", 
-                           overlaying = "y"
-                           )
-      ) 
+    plot_ly(
+      data,
+      y = ~ PricePerLitre,
+      boxpoints = "all",
+      jitter = 0.1,
+      name = "Price per litre (€)",
+      notched = TRUE,
+      type = "box"
+    ) %>%
+      add_boxplot(
+        y = ~ Volume,
+        yaxis = "y2",
+        boxpoints = "all",
+        jitter = 0.1,
+        name = "Volume (l)",
+        notched = TRUE
+      ) %>%
+      layout(
+        title = "Box plots",
+        showlegend = FALSE,
+        yaxis = list(
+          title = "",
+          range = list(ymin1BoxPlot, ymax1BoxPlot)
+        ),
+        yaxis2 = list(
+          title = "",
+          range = list(ymin2BoxPlot, ymax2BoxPlot),
+          side = "right",
+          overlaying = "y"
+        )
+      )
   })
-
+  
   output$barPlot <- renderPlotly({
     plot_ly(
       data %>%
@@ -92,21 +93,19 @@ server <- function(input, output) {
           TotalCost = sum(TotalCost),
           Volume = sum(Volume)
         ),
-      x = ~Day,
-      y = ~TotalCost,
-      text = ~n,
+      x = ~ Day,
+      y = ~ TotalCost,
+      text = ~ n,
       textposition = "outside",
       type = "bar",
       name = "Cost (€)"
     ) %>%
-      add_trace(
-        y = ~Volume,
-        text = "", # Needed, otherwise ~n is printed here as well
-        name = "Volume (l)"
-      ) %>%
+      add_trace(y = ~ Volume,
+                text = "",
+                # Needed, otherwise ~n is printed here as well
+                name = "Volume (l)") %>%
       layout(title = "Distribution of refills",
-             yaxis = list(title = "")
-             )
+             yaxis = list(title = ""))
   })
 }
 
